2b96838fbb41838cdbd15972e5bd7a861ff93de6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 15:52:31 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,10 @@ colA = [4,3,2,1]
 colB = [7,8,9,10]
 
 colsAB = hcat(colA, colB)
+
+colsABC = reshape(colsAB, (2,2,2))
+
+
 df = DataFrame(ColA = colA, ColB = colB)
 
 dates = collect((Date(Dates.now())-Day(3)):Day(1):Date(Dates.now()))
@@ -14,6 +18,8 @@ ta = TimeArray(dates, colsAB, ["ColA","ColB"])
 @test getcolumn(colB, 1)    == colB
 @test getcolumn(colsAB, 1)  == colA
 @test getcolumn(colsAB, 2)  == colB
+@test getcolumn(colsABC, (1,2,0)) == colsABC[1,2,:]
+@test getcolumn(colsABC, (2,0,1)) == colsABC[2,:,1]
 @test getcolumn(df, "ColA") == colA
 @test getcolumn(df, "ColB") == colB
 @test getcolumn(ta, "ColA") == colA
