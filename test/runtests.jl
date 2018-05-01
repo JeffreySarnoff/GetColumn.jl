503d39845652032146752f16f45be4a689f40754
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 15:12:44 -0400

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,20 @@
+using DataFrames, TimeSeries, GetColumn
+using Test
+
+colA = [4,3,2,1]
+colB = [7,8,9,10]
+
+colsAB = hcat(colA, colB)
+df = DataFrame(ColA = colA, ColB = colB)
+
+dates = collect((Date(Dates.now())-Day(3)):Day(1):Date(Dates.now()));
+ta = TimeArray(dates, colsAB, ["ColA","ColB"])
+
+@test getcolumn(colA, 1) === colA
+@test getcolumn(colsAB, 1) === colA
+@test getcolumn(colsAB, 2) === colB
+@test getcolumn(df, "ColA") === colA
+@test getcolumn(df, "ColB") === colB
+@test getcolumn(ta, "ColA") === colA
+@test getcolumn(ta, "ColB") === colB
+
