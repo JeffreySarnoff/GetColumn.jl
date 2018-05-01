27acca724400fcd3e4673ed7546952d42f2792c6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 15:35:16 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using DataFrames, TimeSeries, GetColumn
+using Dates, DataFrames, TimeSeries, GetColumn
 using Test
 
 colA = [4,3,2,1]
@@ -7,14 +7,15 @@ colB = [7,8,9,10]
 colsAB = hcat(colA, colB)
 df = DataFrame(ColA = colA, ColB = colB)
 
-dates = collect((Date(Dates.now())-Day(3)):Day(1):Date(Dates.now()));
+dates = collect((Date(Dates.now())-Day(3)):Day(1):Date(Dates.now()))
 ta = TimeArray(dates, colsAB, ["ColA","ColB"])
 
-@test getcolumn(colA, 1) === colA
-@test getcolumn(colsAB, 1) === colA
-@test getcolumn(colsAB, 2) === colB
-@test getcolumn(df, "ColA") === colA
-@test getcolumn(df, "ColB") === colB
-@test getcolumn(ta, "ColA") === colA
-@test getcolumn(ta, "ColB") === colB
+@test getcolumn(colA, 1)    == colA
+@test getcolumn(colB, 1)    == colB
+@test getcolumn(colsAB, 1)  == colA
+@test getcolumn(colsAB, 2)  == colB
+@test getcolumn(df, "ColA") == colA
+@test getcolumn(df, "ColB") == colB
+@test getcolumn(ta, "ColA") == colA
+@test getcolumn(ta, "ColB") == colB
 
